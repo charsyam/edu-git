e5fa0b88fb2fe13faff70bd0ab52f24861d42dfe
charsyam <charsyam@naver.com>
2024-11-10 10:56:11 +0000

    init commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from add import add
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('calu.html', result=0)
+
+
+@app.route('/add', methods=['POST'])
+def add_endpoint():
+    num1 = int(request.form['num1'])
+    num2 = int(request.form['num2'])
+
+    return render_template('calu.html', result=add(num1,num2))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
